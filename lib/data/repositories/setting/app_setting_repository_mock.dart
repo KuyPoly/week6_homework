882c17bcb8955e7767305d05fb5124667c972216
KuyPoly <kuypoly297@gmail.com>
2026-03-01 22:30:20 +0700

    submit week 6 homwork

---
+++ lib/data/repositories/setting/app_setting_repository_mock.dart
@@ -0,0 +1,15 @@
+import 'app_setting_repository.dart';
+import '../../../model/settings/app_settings.dart';
+
+class AppSettingRepositoryMock implements AppSettingRepository{
+  @override
+  Future<AppSettings> load() {
+    return Future.value(AppSettings(themeColor: ThemeColor.green, darkMode: true, explicitContentEnable: true));
+  }
+
+  @override
+  Future<void> save(AppSettings settings) {
+    return Future.value();
+  }
+  
+}
---
+++ lib/ui/states/settings_state.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/widgets.dart';
+import '../../data/repositories/setting/app_setting_repository_mock.dart';
+import '../../model/settings/app_settings.dart';
+
+class AppSettingsState extends ChangeNotifier {
+  AppSettingsState(this.repository) {
+    init();
+  }
+  final AppSettingRepositoryMock repository;
+  AppSettings? _appSettings;
+  bool get isDarkMode => _appSettings?.darkMode ?? false;
+  bool get isExplicitEnabled => _appSettings?.explicitContentEnable ?? false;
+  Future<void> init() async {
+    // Might be used to load data from repository
+    _appSettings = await repository.load();
+    notifyListeners();
+  }
+
+  ThemeColor get theme => _appSettings?.themeColor ?? ThemeColor.blue;
+
+  Future<void> changeTheme(ThemeColor themeColor) async {
+    if (_appSettings == null) return;
+    _appSettings = _appSettings!.copyWith(themeColor: themeColor);
+    await repository.save(_appSettings!);
+    notifyListeners();
+  }
+
+  Future<void> toggleDarkMode() async {
+    if (_appSettings == null) return;
+    _appSettings = _appSettings!.copyWith(darkMode: !_appSettings!.darkMode);
+    await repository.save(_appSettings!);
+    notifyListeners();
+  }
+
+  Future<void> toggleExplicit() async {
+    if (_appSettings == null) return;
+    _appSettings = _appSettings!.copyWith(explicitContentEnable: !_appSettings!.explicitContentEnable);
+    await repository.save(_appSettings!);
+    notifyListeners();
+  }
+}
